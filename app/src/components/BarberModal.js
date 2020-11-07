b6b36ceb82becc751f3d15eb1a1acd8399363342
AloisioFernandes <aloisio.fernandes@outlook.com>
2020-11-07 13:08:27 -0300

    Agendar serviço

--- app/src/components/BarberModal.js
+++ app/src/components/BarberModal.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components/native'
 import { useNavigation } from '@react-navigation/native'
 
+import ExpandIcon from '../assets/expand.svg'
+
 const Modal = styled.Modal``;
 
 const ModalArea = styled.View`
@@ -18,7 +20,58 @@ const ModalBody = styled.View`
   padding: 10px 20px 40px 20px;
 `;
 
+const CloseButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+`;
+
+const ModalItem = styled.View`
+  background-color: #fff;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  padding: 10px;
+`;
+
+const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 20px;
+  margin-right: 15px;
+`;
+
+const UserName = styled.Text`
+  color: #000;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const ServiceInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ServiceName = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ServicePrice = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
 export default ({ show, setShow, user, service }) => {
+  const navigation = useNavigation()
+
+  const handleCloseButton = () => {
+    setShow(false)
+  }
+
   return (
     <Modal
       transparent={true}
@@ -27,6 +80,25 @@ export default ({ show, setShow, user, service }) => {
     >
       <ModalArea>
         <ModalBody>
+          <CloseButton onPress={handleCloseButton}>
+            <ExpandIcon width="40" height="40" fill="#000" />
+          </CloseButton>
+
+          <ModalItem>
+            <UserInfo>
+              <UserAvatar source={{uri: user.avatar}} />
+              <UserName>{user.name}</UserName>
+            </UserInfo>
+          </ModalItem>
+
+          {service !== null &&
+            <ModalItem>
+              <ServiceInfo>
+                <ServiceName>{user.services[service].name}</ServiceName>
+                <ServicePrice>R$ {user.services[service].price.toFixed(2)}</ServicePrice>
+              </ServiceInfo>
+            </ModalItem>
+          }
 
         </ModalBody>
       </ModalArea>
